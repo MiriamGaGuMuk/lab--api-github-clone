4b377ac1958ddbbc91561e8a0c1a71f6058064c2
MiriamGaGuMuk <mgg_pjl@yahoo.com>
2018-12-09 06:33:09 -0600

    DOM done, filter, styles with sass

--- src/js/app.js
+++ src/js/app.js
@@ -4,21 +4,29 @@ const request = superagent
 
 
 let name = document.querySelector('.profile__name')
-let aka = document.querySelector('.profile__nickName')
+let nick = document.querySelector('.profile__nickName')
 let imageAvatar = document.querySelector('.profile__photo')
-let bio = document.querySelector('.bio')
+let bio = document.querySelector('.profile__bio')
 let company = document.querySelector('.company')
 let city = document.querySelector('.location')
 let email = document.querySelector('.email')
 let blog = document.querySelector('.blog')
-let container = document.querySelector('.containerright')
+let container = document.querySelector('.rightContainer')
 let input = document.querySelector('.textInput')
 
-input.document.addEventListener('') {}
+let repos = "";
 
+input.addEventListener("keypress", function(e){
+    let inputUsername = ""
+    if(e.keyCode == 13)
+    {
+        inputUsername = input.value // returning false will prevent the event from bubbling up.
+        filterBox(inputUsername)
+    }
 
-let reposVar = "";
-
+    console.log(inputUsername)
+})
+// console.log(input.value)
 
 const url = 'https://api.github.com/users/MiriamGaGuMuk'
 
@@ -30,24 +38,21 @@ request
         //console.log(data.body)
         name.textContent = data.body.login
         imageAvatar.src = data.body.avatar_url
-        aka.textContent = data.body.name
+        nick.textContent = data.body.name
         bio.textContent = data.body.bio
         company.textContent = data.body.company
         city.textContent = data.body.location
         email.textContent = data.body.email
         blog.textContent = data.body.blog
 
-
-        //console.log(data.body.results[0].email)
-
     })
 
-const reposUrl = 'https://api.github.com/users/MiriamGaGuMuk/repos'
+const urlRepo = 'https://api.github.com/users/MiriamGaGuMuk/repos'
 request
-    .get(reposUrl)
+    .get(urlRepo)
     .then(function (data) {
         repos = data.body
-        // console.log(reposVar)
+        // console.log(repos)
 
         let templete = "";
 
@@ -63,4 +68,51 @@ request
         container.innerHTML = templete
 
 
-    })
+    });
+    
+    
+    function filterBox(event){
+    const url = `https://api.github.com/users/${event}`
+    request
+        .get(url)
+        .then(function(data){
+                
+                //console.log(data.body)
+                name.textContent = data.body.login
+                imageAvatar.src = data.body.avatar_url
+                nick.textContent = data.body.name
+                bio.textContent = data.body.bio
+                company.textContent = data.body.company
+                city.textContent = data.body.location
+                email.textContent = data.body.email
+                blog.textContent = data.body.blog
+                //console.log(data.body.results[0].email)
+                
+        });
+    
+    const url_repos = `https://api.github.com/users/${event}/repos`
+        request
+        .get(url_repos)
+        .then(function(data){
+            repos = data.body
+            // console.log(repos)
+    
+            let templete =""
+            
+                
+                repos.forEach(function(element){
+                    templete += `<article class="repo">
+                                    <h2 class="repo__title">${element.name}</h2>
+                                    <h3 class="repo__description">${element.description}</h3>
+                                    <p class="repo__all">${element.language} | ${element.stargazers_count} | ${element.updated_at}</p>
+                                </article>`
+                })
+    
+                container.innerHTML = templete
+                
+        });
+
+    }
+    
+    
+    
